00b5881a5ce28dab13c64c2f776b8d5cc11a352a
Fabricio Silva <fabricio.silva.s.silva@gmail.com>
2024-01-21 22:04:26 -0300

    prevenção de erros do form vendas

--- src/renderer.js
+++ src/renderer.js
@@ -29,48 +29,85 @@ document.addEventListener('DOMContentLoaded', (event) => {
     return `${dataLocal} ${horaLocal}`;
   }
 
+// form vendas ↓↓↓
+const vendaForm = document.querySelector('#vendaForm');
+if (vendaForm) 
+{
+    vendaForm.addEventListener('submit', (event) => 
+{
+  event.preventDefault();
 
-  //fomulario de vendas  ↓  ↓  ↓
-  const vendaForm = document.querySelector('#vendaForm');
-  if (vendaForm) 
-  {
-      vendaForm.addEventListener('submit', (event) => 
-  {
-    event.preventDefault();
+  // Obtém os valores do formulário das vendas
+  const cliente = document.getElementById('cliente').value;
+  const tipo = document.getElementById('tipo').value;
+  const descricao = document.getElementById('descricao').value;
+  const preco = parseFloat(document.getElementById('preco').value); 
+  const quantidade = document.getElementById('quantidade').value;
+  const dataVenda = getDataHoraLocal(); 
+  const tipos = document.getElementById('tipo').value;
+  const metodoPagamento = document.getElementById('pagamento').value;
 
-    // Obtém os valores do formulário das vendas
-    const cliente = document.getElementById('cliente').value;
-    const tipo = document.getElementById('tipo').value;
-    const descricao = document.getElementById('descricao').value;
-    const preco = parseFloat(document.getElementById('preco').value); 
-    const quantidade = document.getElementById('quantidade').value;
-    const dataVenda = getDataHoraLocal(); 
-    const tipos = document.getElementById('tipo').value;
-    const metodoPagamento = document.getElementById('pagamento').value;
-
-    let dados;
-    if (tipos === 'roupa') {
-      const genero = document.getElementById('genero').value;
-      const marca = document.getElementById('marca').value;
-      const categoria = document.getElementById('categoria').value;
-      // Inclui genero e marca nos dados enviados
-      dados = { cliente, tipo, genero, categoria, marca, metodoPagamento, descricao, preco, quantidade, dataVenda };
-    } else if (tipos === 'objeto') {
-      // Exclui genero e marca dos dados enviados
-      dados = { cliente, tipo, descricao, metodoPagamento, preco, quantidade, dataVenda };
+  // Verifica se todos os campos estão preenchidos
+  if (!cliente || !tipo || !pagamento || !preco || !quantidade) {
+    showModal('Por favor, preencha todos os campos.');
+    return; // Impede o envio do formulário
+  } else if (tipos === 'roupa') {
+    const genero = document.getElementById('genero').value;
+    const marca = document.getElementById('marca').value;
+    const categoria = document.getElementById('categoria').value;
+    if (!genero || !categoria || !marca) {
+      showModal('Por favor, preencha todos os campos para a venda de roupas.');
+      return; // Impede o envio do formulário
     }
-
-    // Se o pagamento for a prazo (Fiado), adiciona o valor da venda à dívida do cliente
-    if (metodoPagamento === 'Fiado') {
-      // Envia um evento IPC para adicionar o valor da venda à dívida do cliente
-      ipcRenderer.send('registrar-divida', { cliente: cliente, divida: preco });
-    }
-
-    // Envia um evento IPC com os valores do formulário
-    ipcRenderer.send('submit-venda', dados);
-    });
-
+  } else if (tipos === 'objeto' && !descricao) {
+    showModal('Por favor, preencha todos os campos para a venda de objetos.');
+    return; // Impede o envio do formulário
   }
+
+  let dados;
+  if (tipos === 'roupa') {
+    const genero = document.getElementById('genero').value;
+    const marca = document.getElementById('marca').value;
+    const categoria = document.getElementById('categoria').value;
+    // Inclui genero e marca nos dados enviados
+    dados = { cliente, tipo, genero, categoria, marca, metodoPagamento, descricao, preco, quantidade, dataVenda };
+  } else if (tipos === 'objeto') {
+    // Exclui genero e marca dos dados enviados
+    dados = { cliente, tipo, descricao, metodoPagamento, preco, quantidade, dataVenda };
+  }
+
+  // Se o pagamento for a prazo (Fiado), adiciona o valor da venda à dívida do cliente
+  if (metodoPagamento === 'Fiado') {
+    // Envia um evento IPC para adicionar o valor da venda à dívida do cliente
+    ipcRenderer.send('registrar-divida', { cliente: cliente, divida: preco });
+  }
+
+  // Envia um evento IPC com os valores do formulário
+  ipcRenderer.send('submit-venda', dados);
+
+  // Exibe uma mensagem de sucesso
+  showModal('Venda Realizada');
+});
+
+// Função para exibir a janela modal
+function showModal(message) {
+  const modal = document.getElementById('modal');
+  const span = document.getElementsByClassName('close-button')[0];
+  document.getElementById('modal-text').textContent = message;
+  modal.style.display = 'block';
+  span.onclick = function() {
+    modal.style.display = 'none';
+  }
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  }
+}
+}
+
+  
+  
 
 });
 
